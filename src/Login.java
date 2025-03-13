f79908191360cf31a4b203934466079954b04dd9
KheangAnn <Kheangann@gmail.com>
2025-03-13 16:20:25 +0700

    fix

--- src/Login.java
+++ src/Login.java
@@ -10,6 +10,8 @@ public class Login extends JFrame implements ActionListener {
     JPasswordField pintextField;
     JLabel text, loginScreen, Card_num, password, background;
 
+    
+
     Login() {
         setLayout(null);
 
