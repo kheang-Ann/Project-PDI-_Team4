43c554db147e4fd250715cd064f59034184d8a07
khem chhun <khemchhun911@gmail.com>
2025-03-13 16:25:12 +0700

    updated

--- src/Transfer.java
+++ src/Transfer.java
@@ -9,8 +9,9 @@ public class Transfer extends JFrame implements ActionListener {
     JLabel text, amount, Card_no;
     JTextField money, card;
     JButton transfer, back;
-    String pin;
-    Transfer(String pin){
+    String pin;    
+
+    Transfer(String pin) {
         this.pin = pin;
 
         setLayout(null);
@@ -24,134 +25,148 @@ public class Transfer extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setResizable(false);
         getContentPane().setBackground(Color.decode("#578FCA"));
-        
+
         text = new JLabel("Transfer");
         text.setFont(new Font("Tahoma", Font.BOLD, 25));
-        text.setBounds(200,10,200,40);
+        text.setBounds(200, 10, 200, 40);
         add(text);
-        
+
         Card_no = new JLabel("Card Number: ");
         Card_no.setFont(new Font("Tahoma", Font.BOLD, 15));
-        Card_no.setBounds(40,100,200,40);
+        Card_no.setBounds(40, 100, 200, 40);
         add(Card_no);
-        
+
         card = new JTextField();
         card.setFont(new Font("Tahoma", Font.BOLD, 15));
         card.setBounds(160, 100, 200, 40);
         add(card);
 
-        card.addKeyListener(new KeyAdapter(){
-            public void keyTyped(KeyEvent e){
+        card.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
                 char c = e.getKeyChar();
-                if(!Character.isDigit(c)){
+                if (!Character.isDigit(c)) {
                     e.consume();
                 }
             }
         });
-        
+
         amount = new JLabel("Amount: ");
         amount.setFont(new Font("Tahoma", Font.BOLD, 15));
-        amount.setBounds(80,150,100,40);
+        amount.setBounds(80, 150, 100, 40);
         add(amount);
-        
+
         money = new JTextField();
         money.setFont(new Font("Tahoma", Font.BOLD, 15));
-        money.setBounds(160,150,200,40);
+        money.setBounds(160, 150, 200, 40);
         money.setColumns(15);
         getContentPane().add(money);
 
-        money.addKeyListener(new KeyAdapter(){
-            public void keyTyped(KeyEvent e){
+        money.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
                 char c = e.getKeyChar();
-                if(!Character.isDigit(c)){
+                if (!Character.isDigit(c)) {
                     e.consume();
                 }
             }
         });
-        
+
         transfer = new JButton("Transfer");
         transfer.setFont(new Font("Tahoma", Font.BOLD, 15));
-        transfer.setBounds(185,220,150,40);
+        transfer.setBounds(185, 220, 150, 40);
         transfer.addActionListener(this);
         transfer.setFocusable(false);
         add(transfer);
-        
+
         back = new JButton("Exit");
         back.setFont(new Font("Tahoma", Font.BOLD, 15));
-        back.setBounds(185,270,150,40);
+        back.setBounds(185, 270, 150, 40);
         back.addActionListener(this);
         back.setFocusable(false);
         add(back);
-        
+
         setVisible(true);
     }
 
-    class verify extends JFrame implements ActionListener{
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == transfer) {
+            String number = money.getText();
+            if (number.equals("")) {
+                JOptionPane.showMessageDialog(null, "Please enter the amount that you want to transfer.");
+            } else {
+                setVisible(false);
+                new verify(pin, number).setVisible(true);
+            }
+        } else if (ae.getSource() == back) {
+            setVisible(false);
+            new Transaction(pin).setVisible(true);
+        }
+    }
+
+    class verify extends JFrame implements ActionListener {
         JButton pins, Back;
-        JTextField fill_pin;
+        JPasswordField fill_pin;
         JLabel pinn, text;
         String code;
+        String amount;
 
-        public verify(String code) {
+        public verify(String code, String amount) {
             this.code = code;
+            this.amount = amount;
 
             setLayout(null);
 
-            text = new JLabel("Verify the pin first before transfer money");
+            text = new JLabel("Verify the pin first before transferring money");
             text.setFont(new Font("Tahoma", Font.BOLD, 15));
-            text.setBounds(30,30,400,20);
+            text.setBounds(30, 30, 400, 20);
             add(text);
 
             pinn = new JLabel("Pin: ");
             pinn.setFont(new Font("Tahoma", Font.BOLD, 15));
-            pinn.setBounds(50,100,40,40);
+            pinn.setBounds(50, 100, 40, 40);
             add(pinn);
 
-            fill_pin = new JTextField();
-            fill_pin.setBounds(100,105,200,30);
+            fill_pin = new JPasswordField();
+            fill_pin.setBounds(100, 105, 200, 30);
             add(fill_pin);
 
             fill_pin.addKeyListener(new KeyAdapter() {
-                public void keyTyped(KeyEvent e){
+                public void keyTyped(KeyEvent e) {
                     char c = e.getKeyChar();
-                    if(!Character.isDigit(c)){
+                    if (!Character.isDigit(c)) {
                         e.consume();
                     }
                 }
             });
 
-
             pins = new JButton("Verify");
-            pins.setBounds(150,150, 100,40);
+            pins.setBounds(150, 150, 100, 40);
             pins.setFocusable(false);
             pins.addActionListener(this);
             add(pins);
 
             Back = new JButton("Back");
-            Back.setBounds(150,200,100,40);
+            Back.setBounds(150, 200, 100, 40);
             Back.setFocusable(false);
             Back.addActionListener(this);
             add(Back);
 
             setTitle("Verify the pin");
-            setSize(400,300);
+            setSize(400, 300);
             setLocation(300, 0);
             setResizable(false);
             setLocationRelativeTo(null);
             setVisible(true);
-
         }
-        public void actionPerformed(ActionEvent ea){
 
-            
-            if (ea.getSource() == Back){
+        public void actionPerformed(ActionEvent ea) {
+            if (ea.getSource() == Back) {
                 setVisible(false);
                 new Transfer(code).setVisible(true);
-            } else if(ea.getSource() == pins){
+            } else if (ea.getSource() == pins) {
                 Bank b = new Bank();
                 String pin = fill_pin.getText();
 
-                if(pin.isEmpty()){
+                if (pin.isEmpty()) {
                     JOptionPane.showMessageDialog(null, "Please enter the pin.");
                     return;
                 }
@@ -160,48 +175,32 @@ public class Transfer extends JFrame implements ActionListener {
                 System.out.println("Executing query: " + query);
                 try {
                     ResultSet set = b.s.executeQuery(query);
-                    if(set.next()){
-                        System.out.println("Pin verify successfully.");
+                    if (set.next()) {
+                        System.out.println("Pin verified successfully.");
                         setVisible(false);
-                        new Transaction(code).setVisible(true);
-                    }
-                    else {
+                        transferMoney();
+                    } else {
                         JOptionPane.showMessageDialog(null, "Incorrect pin number!");
                     }
                 } catch (Exception ex) {
-                    System.out.println( "Exception: " +ex);
+                    System.out.println("Exception: " + ex);
                     JOptionPane.showMessageDialog(null, "An error occurred while verifying the pin.");
                 }
             }
         }
-        
-    }
-    
-    public void actionPerformed(ActionEvent ae){
-        if(ae.getSource() == transfer){
-            setVisible(false);
-            new verify(pin).setVisible(true);
 
-
-            String number = money.getText();
-            Date date = new Date();
-            if(number.equals("")){
-                JOptionPane.showMessageDialog(null, "Please enter amount that you want to withdraw.");
-            } else {
-                try {
-                    Bank bank = new Bank();
-                    String query = "insert into bank value('" +pin+"', '" + date + "', 'Withdraw' , '" +number+ "')";
-                    bank.s.executeUpdate(query);
-                    JOptionPane.showMessageDialog(null, "USD "+number+" Withdraw successfully");
-                    setVisible(false);
-                    new Transaction(pin).setVisible(true);
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
+        private void transferMoney() {
+            try {
+                Bank bank = new Bank();
+                Date date = new Date();
+                String query = "insert into bank value('" + code + "', '" + date + "', 'Transfered' , '" + amount
+                        + "')";
+                bank.s.executeUpdate(query);
+                JOptionPane.showMessageDialog(null, "USD " + amount + " transferred successfully");
+                new Transaction(code).setVisible(true);
+            } catch (Exception e) {
+                System.out.println(e);
             }
-        }else if (ae.getSource() == back){
-            setVisible(false);
-            new Transaction(pin).setVisible(true);
         }
     }
 
--- src/signupTwo.java
+++ src/signupTwo.java
@@ -1,240 +1,199 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 import java.util.*;
 import javax.swing.*;
 
-public class signupTwo extends JFrame implements ActionListener{
-    JTextField idCard, ph, Senior;
-    JButton Submit, cancel;
-    JRadioButton yes, no , yes2, no2;
-    JComboBox incomes, educations, job, reli, countrys;
-    JLabel Existing, page2, country, religion, income, education, quailification, Job, phone, IDcard;
+public class signupTwo extends JFrame implements ActionListener {
+    JTextField idCard, ph;
+    JButton submit, cancel;
+    JRadioButton yes, no, yes2, no2;
+    JComboBox<String> incomes, educations, job, reli, countrys;
     String form;
 
-    signupTwo(String form){
+    signupTwo(String form) {
         this.form = form;
-
         setLayout(null);
 
-        
         setSize(850, 800);
         setLocation(350, 10);
         setTitle("Sign Up");
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        ImageIcon I1 = new ImageIcon("C:\\Users\\ASUS\\OneDrive\\Pictures\\Saved Pictures\\Logo.png");
-        Image I2 = I1.getImage().getScaledInstance(100, 100, DO_NOTHING_ON_CLOSE);
-        ImageIcon I3 = new ImageIcon(I2);
-        JLabel label = new JLabel(I3);
-        label.setBounds(60, 10, 100, 100);
-        add(label);
-        
-        setTitle("NEW ACCOUNT APPLICATION FORM - PAGE 2 ");
-        
-        
-        page2 = new JLabel("Page 2: Additional Details");
+
+        JLabel page2 = new JLabel("Page 2: Additional Details");
         page2.setFont(new Font("Tahoma", Font.BOLD, 20));
-        page2.setBounds(300, 80,400,40);
+        page2.setBounds(300, 80, 400, 40);
         add(page2);
-        
-        country = new JLabel("Country: "); //Religion
-        country.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        country.setBounds(50, 150, 100, 40);
-        add(country);
-        
-        String countries[] = {"Cambodia", "Thailand"," Vietnam"," Malaysia", " Laos", "Singapore","Myanmar", "Indonesia"};
-        countrys = new JComboBox(countries);
-        countrys.setBounds(200,160,444,25);
+
+        JLabel countryLabel = new JLabel("Country: ");
+        countryLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        countryLabel.setBounds(50, 150, 100, 40);
+        add(countryLabel);
+
+        String[] countries = { "Cambodia", "Thailand", "Vietnam", "Malaysia", "Laos", "Singapore", "Myanmar",
+                "Indonesia" };
+        countrys = new JComboBox<>(countries);
+        countrys.setBounds(200, 160, 444, 25);
         add(countrys);
-        
-        religion = new JLabel("Religions: ");       //Category
-        religion.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        religion.setBounds(50,200,200,40);
-        add(religion);
-        
-        String religions[] = {"General","Islam", "Hinduism", "Buddhism", "Christianity"};
-        reli = new JComboBox(religions);
-        reli.setBounds(200,210, 444,25);
+
+        JLabel religionLabel = new JLabel("Religion: ");
+        religionLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        religionLabel.setBounds(50, 200, 200, 40);
+        add(religionLabel);
+
+        String[] religions = { "General", "Islam", "Hinduism", "Buddhism", "Christianity" };
+        reli = new JComboBox<>(religions);
+        reli.setBounds(200, 210, 444, 25);
         add(reli);
-        
-        
-        income = new JLabel("Income: ");    //Income
-        income.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        income.setBounds(50,250,200,40);
-        add(income);
-        
-        String Income[] = {"Null","< 500 $", "< 5,000 $  ", "< 5,000,000 $ "};
-        incomes = new JComboBox(Income);
-        incomes.setBounds(200,260,444,25);
+
+        JLabel incomeLabel = new JLabel("Income: ");
+        incomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        incomeLabel.setBounds(50, 250, 200, 40);
+        add(incomeLabel);
+
+        String[] Income = { "Null", "< 500 $", "< 5,000 $", "< 5,000,000 $" };
+        incomes = new JComboBox<>(Income);
+        incomes.setBounds(200, 260, 444, 25);
         add(incomes);
-        
-        education = new JLabel("Educational ");    // Education
-        education.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        education.setBounds(50,300,200,40);
-        add(education);
-        
-        String edu[] = {"High School","Bachelor Degree", "Graduated ", "Just a chill guy"};
-        educations = new JComboBox(edu);
-        educations.setBounds(200,320,444,25);
+
+        JLabel educationLabel = new JLabel("Educational : ");
+        educationLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        educationLabel.setBounds(50, 300, 200, 40);
+        add(educationLabel);
+
+        String[] edu = { "High School", "Bachelor Degree", "Graduated", "Just a chill guy" };
+        educations = new JComboBox<>(edu);
+        educations.setBounds(200, 320, 444, 25);
         add(educations);
-        
-        quailification = new JLabel("Quailifcation ");
-        quailification.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        quailification.setBounds(50,320,200,40);
-        add(quailification);
-        
-        Job = new JLabel("Job: ");   //job
-        Job.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        Job.setBounds(50,385,200,40);
-        add(Job);
-        
-        String jobs[] = {"Saller","Self Employed", "Bussiness", "Student", "Other"};
-        job = new JComboBox(jobs);
-        job.setBounds(200,390,444,25);
+
+        JLabel jobLabel = new JLabel("Job: ");
+        jobLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        jobLabel.setBounds(50, 385, 200, 40);
+        add(jobLabel);
+
+        String[] jobs = { "Seller", "Self Employed", "Business", "Student", "Other" };
+        job = new JComboBox<>(jobs);
+        job.setBounds(200, 390, 444, 25);
         add(job);
-        
-        phone = new JLabel("Phone Number: ");        //Phone number
-        phone.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        phone.setBounds(50,450,200,40);
-        add(phone);
+
+        JLabel phoneLabel = new JLabel("Phone Number: ");
+        phoneLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        phoneLabel.setBounds(50, 450, 200, 40);
+        add(phoneLabel);
 
         ph = new JTextField();
-        ph.setBounds(200,460, 444,25);
+        ph.setBounds(200, 460, 444, 25);
         ph.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        ph.setColumns(15);
         add(ph);
-        
-        IDcard = new JLabel("National ID card: ");     //National ID card
-        IDcard.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        IDcard.setBounds(50,500,200,40);
-        add(IDcard);
+
+        JLabel IDcardLabel = new JLabel("National ID Card: ");
+        IDcardLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        IDcardLabel.setBounds(50, 500, 200, 40);
+        add(IDcardLabel);
 
         idCard = new JTextField();
-        idCard.setBounds(200,510, 444,25);
+        idCard.setBounds(200, 510, 444, 25);
         idCard.setFont(new Font("Tahoma", Font.PLAIN, 15));
         add(idCard);
-        idCard.setColumns(15);
-        
-        JLabel Senior = new JLabel("Senior Citizen(60 up): ");      //Senior Citizen
-        Senior.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        Senior.setBounds(50,550,200,40);
-        add(Senior);
-        
+
+        JLabel seniorLabel = new JLabel("Senior Citizen (60+): ");
+        seniorLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        seniorLabel.setBounds(50, 550, 200, 40);
+        add(seniorLabel);
+
         yes = new JRadioButton("Yes");
-        yes.setBounds(250,550,100,40);
-        add(yes);
-        
         no = new JRadioButton("No");
-        no.setBounds(350,550,100,40);
+        ButtonGroup seniorGroup = new ButtonGroup();
+        seniorGroup.add(yes);
+        seniorGroup.add(no);
+
+        yes.setBounds(250, 550, 100, 40);
+        no.setBounds(350, 550, 100, 40);
+        add(yes);
         add(no);
-        
-        ButtonGroup citi = new ButtonGroup();
-        citi.add(yes);
-        citi.add(no);
-        
-        
-        Existing = new JLabel("Existing Account: ");
-        Existing.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        Existing.setBounds(50,600,200,40);
-        add(Existing);
-        
+
+        JLabel existingLabel = new JLabel("Existing Account: ");
+        existingLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        existingLabel.setBounds(50, 600, 200, 40);
+        add(existingLabel);
+
         yes2 = new JRadioButton("Yes");
-        yes2.setBounds(250,600,100,40);
-        add(yes2);
-        
         no2 = new JRadioButton("No");
-        no2.setBounds(350,600,100,40);
+        ButtonGroup existingGroup = new ButtonGroup();
+        existingGroup.add(yes2);
+        existingGroup.add(no2);
+
+        yes2.setBounds(250, 600, 100, 40);
+        no2.setBounds(350, 600, 100, 40);
+        add(yes2);
         add(no2);
-        
-        ButtonGroup exist = new ButtonGroup();
-        exist.add(yes2);
-        exist.add(no2);
-        
+
         cancel = new JButton("Cancel");
-        cancel.setFont(new Font("Raleway", Font.BOLD,14));
-        cancel.setBounds(420,670,100,40);
+        cancel.setBounds(420, 670, 100, 40);
         cancel.addActionListener(this);
         add(cancel);
-        
-        //button next
-        Submit = new JButton("Submit");
-        Submit.setBounds(570,670,100,40);
-        Submit.addActionListener(this);
-        add(Submit);
 
-        ph.addKeyListener(new KeyAdapter(){
-            public void keyTyped(KeyEvent e){
-                char c = e.getKeyChar();
-                if(!Character.isDigit(c)){
-                    e.consume();
-                }
-            }
-        });
+        submit = new JButton("Submit");
+        submit.setBounds(570, 670, 100, 40);
+        submit.addActionListener(this);
+        add(submit);
 
-        idCard.addKeyListener(new KeyAdapter(){
-            public void keyTyped(KeyEvent e){
-                char c = e.getKeyChar();
-                if(!Character.isDigit(c)){
-                    e.consume();
-                }
-            }
-        });
-        
-        setVisible(true); 
-        }
+        setVisible(true);
+    }
 
-    public void actionPerformed(ActionEvent as){
-        String countryy = (String) countrys.getSelectedItem(); //setText
-        String Sreligion = (String) reli.getSelectedItem();
-        String sincome = (String) incomes.getSelectedItem();
-        String Edu = (String) educations.getSelectedItem();
-        String jobss = (String) job.getSelectedItem();
-        String citi = null;
-        if (yes.isSelected()){
-            citi = "Yes";
-        } else if (no.isSelected()){
-            citi = "No";
-        }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submit) {
+            String country = (String) countrys.getSelectedItem();
+            String Sreligion = (String) reli.getSelectedItem();
+            String sincome = (String) incomes.getSelectedItem();
+            String Edu = (String) educations.getSelectedItem();
+            String jobss = (String) job.getSelectedItem();
+            String senior = yes.isSelected() ? "Yes" : "No";
+            String existingAccount = yes2.isSelected() ? "Yes" : "No";
+            String phone = ph.getText();
+            String id = idCard.getText();
 
-        String exist = null;
-        if(yes2.isSelected()){
-            exist = "Yes";
-        } else if (no2.isSelected()){
-            exist = "No";
-        }
+            String cardNumber = "" + (1000000 + new Random().nextInt(9000000)); // Ensures 7-digit number
+            String pinNumber = "" + (100 + new Random().nextInt(900)); // Ensures 3-digit number
 
-        String span = ph.getText();
-        String card = idCard.getText();
+            try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem",
+                    "root", "khemchhun250306")) {
 
-        if(as.getSource() == Submit){
-            Random random = new Random();
-            String cardnumber = "" + Math.abs((random.nextLong() % 9000000L));
-    
-            String pinnumber = "" + Math.abs((random.nextLong() % 900L));
-            try {
-                
-                Bank c = new Bank();
-                String query =  "insert into signupTwo value('" +form +"', '" + countryy + "', '" + Sreligion + "', '" + sincome + "', '" + Edu + "', '" + jobss + "', '" + span + "', '" + card + "', '" + citi + "', '" + exist + "')";
-                String login = "insert into login value('" +form +"', '" + cardnumber + "', '" + pinnumber + "')";
-                
-                c.s.executeUpdate(query);
-                c.s.executeUpdate(login);
-    
-                JOptionPane.showMessageDialog(null, "Card number " + cardnumber + "\n Pin " + pinnumber);   
-    
+                String query1 = "INSERT INTO signupTwo (form, country, Sreligion, Income, Education, jobss, Pan, ID, SeniorCitizen, ExistingAccount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement ps1 = conn.prepareStatement(query1);
+                ps1.setString(1, form);
+                ps1.setString(2, country);
+                ps1.setString(3, Sreligion);
+                ps1.setString(4, sincome);
+                ps1.setString(5, Edu);
+                ps1.setString(6, jobss);
+                ps1.setString(7, phone);
+                ps1.setString(8, id);
+                ps1.setString(9, senior);
+                ps1.setString(10, existingAccount);
+                ps1.executeUpdate();
+
+                String query2 = "CALL insert_pin(?, ?, ?)";
+                PreparedStatement ps2 = conn.prepareStatement(query2);
+                ps2.setString(1, pinNumber);
+                ps2.setString(2, form);
+                ps2.setString(3, cardNumber);
+                ps2.executeUpdate();
+
+                JOptionPane.showMessageDialog(null, "Card number: " + cardNumber + "\nPIN: " + pinNumber);
                 setVisible(false);
                 new Transaction(form).setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
-            
-        }
-        else if(as.getSource() == cancel){
+        } else if (e.getSource() == cancel) {
             setVisible(false);
             new Login().setVisible(true);
         }
     }
+
     public static void main(String[] args) {
         new signupTwo("");
     }
